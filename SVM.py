4f9d2dc4e658d938e10b238a423cdf3841ebe176
GijsOW <90450155+GijsOW@users.noreply.github.com>
2023-02-05 15:17:12 +0100

    Add files via upload

---
+++ SVM.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[54]:
+
+
+import struct
+import numpy as np
+from sklearn import svm, metrics
+from sklearn.model_selection import train_test_split, cross_val_predict
+
+import matplotlib.pyplot as plt
+import os
+import random as rd
+import re
+import pandas as pd
+import itertools
+import math
+
+
+# In[47]:
+
+
+def read_data_svm(path):  #reads the data the same way as CNN but without reshaping
+    with open(path) as f:
+        i = 0
+        data = np.zeros((8000,240))
+        for line in f:
+            digits = re.findall("\d+\.\d+", line)
+            for j in range(len(digits)):
+                data[i][j] = pd.to_numeric(digits[j])
+            i = i + 1 
+            
+    return(data)
+
+def gen_labels(size):
+    labels = [0]*size
+    for i in range(len(labels)):
+            labels[i] = math.floor(i/200)
+            labels[i] = labels[i] - (math.floor(i/2000)*10)
+    return labels
+
+def plot_confusion(y_test, y_pred):
+    # Compute and print the confusion matrix
+    cm = metrics.confusion_matrix(y_test, y_pred)
+    print("Confusion matrix:")
+    print(cm)
+
+    # Plot the confusion matrix
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.colorbar()
+    tick_marks = np.arange(10)
+    plt.xticks(tick_marks, range(10))
+    plt.yticks(tick_marks, range(10))
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.show()
+    
+
+
+# In[56]:
+
+
+def main():
+#     os.chdir(os.path.dirname(sys.argv[0]))
+    
+    data = read_data("final.txt")
+    labels = gen_labels(len(data))
+    X_train, X_test,y_train, y_test = train_test_split(data,labels,
+                                   random_state=94, 
+                                   test_size=0.25, 
+                                   shuffle=True)
+    y_train = np.array(y_train)
+    y_test = np.array(y_test)
+
+    modelSVM = svm.SVC(kernel = 'poly',C=1, gamma = 0.5, degree=1).fit(X_train, y_train)
+    
+    # Use cross-validation to make predictions on the test data
+    y_pred = cross_val_predict(modelSVM, X_test, y_test, cv=5)
+
+    # Compute and print the accuracy
+    acc = metrics.accuracy_score(y_test, y_pred)
+    print("Accuracy:", acc)
+    
+    plot_confusion(y_test, y_pred)
+    
+    return
+
+if __name__ == "__main__":
+    main()
+
